137f5545a4cadc516b07b91692e4384685c8624b
Ilham Fardianysah <93816433+Ilhamfardiansyah@users.noreply.github.com>
2022-07-21 18:57:43 +0700

    update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,7 +19,7 @@ class UserSeeder extends Seeder
             'nik' => '15128447',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('12345'),
-            'image' => 'user2-160x160'
+            'image' => '2c8e3a4e1d7311d7fbd7fd4cef5871eb.jpg'
         ]);
         $admin->assignRole('admin');
 
